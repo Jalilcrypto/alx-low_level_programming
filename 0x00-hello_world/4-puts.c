ef1ca74d307cee3fe2f66892997172f43dac61c1
jalil <90767604+Jalilcrypto@users.noreply.github.com>
2022-09-09 00:37:22 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - printing using puts
  *
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+        puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
