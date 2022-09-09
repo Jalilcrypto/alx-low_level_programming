bf11c1189b6fd341b824da0223d878a1b1179aed
jalil <90767604+Jalilcrypto@users.noreply.github.com>
2022-09-09 01:55:46 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+* main - print out string.
 * Description: main print out string using print.
 * Return: 0 if successful
 */
@@ -8,5 +9,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	retun (0);
+	retun(0);
 }
