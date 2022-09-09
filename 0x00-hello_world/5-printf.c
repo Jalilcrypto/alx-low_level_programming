ff42d1814a83bc8dd2cf72a73d902138630d27bf
jalilcrypto <rabiua05@gmail.com>
2022-09-08 17:45:38 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* Description: main print out string using print.
+* Return: 0 if successful
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun (0);
+}
