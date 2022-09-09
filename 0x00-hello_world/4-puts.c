913972bd8524293ed5adf71d8239da07e1a98338
jalil <90767604+Jalilcrypto@users.noreply.github.com>
2022-09-09 01:21:43 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,9 @@
 
 /**
 * main - print string using the put function
-* 
+*
+* description: using the main fuction 
+* printm "Programming is like building a multilingual puzzle" 
 * return: always 0 (success)
 */
 int main(void)
