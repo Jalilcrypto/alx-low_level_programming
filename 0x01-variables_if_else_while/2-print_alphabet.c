5c25f5f0271fd3eaa0567f97f6a42fbbf2d52f01
jalil <90767604+Jalilcrypto@users.noreply.github.com>
2022-09-10 00:00:12 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,34 +4,18 @@
 
 #include <stdio.h>
 
-
-
 /**
-
- * main - Entry point
-
- *
-
+ * main - print alphabet in lowercase
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
-
 char c;
-
 for (c = 'a'; c <= 'z'; c++)
-
 {
-
 putchar(c);
-
 }
-
 putchar('\n');
-
 return (0);
-
 }
