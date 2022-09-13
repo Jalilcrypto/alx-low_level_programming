5058a16e497656018a74777f5dd795dd2b9105b4
jalil <90767604+Jalilcrypto@users.noreply.github.com>
2022-09-13 22:40:29 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,12 @@ int main(void)
 {
 	char putchar[] = "_putchar";
 	int i = 0
+		
 	while (putchar[i] != '\0')
 	{
 		_putchar(putchar[i]);
 		i++
 	}
-	_putchar ('\n')
-	return (0)
+	_putchar ('\n');
+	return (0);
 }
